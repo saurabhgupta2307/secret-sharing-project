300b85b16c8c51981f17198658417f8839be1e4d
Saurabh Gupta <gupta.saurabh2307@gmail.com>
2015-11-30 23:45:01 -0700

    minor fixes

--- demo.py
+++ demo.py
@@ -245,7 +245,7 @@ if __name__ == "__main__":		#code to execute if called from command-line
 
 	secretNum = message.strToNum(secret)
 	prime = getLargePrime(secretNum)
-	key = generatekey(512)
+	key = generatekey(256)
 	buf = 1024
 
 	minPort = random.randint(10000, 30000)
